33b70f08184a46eeefd39700f3bcdf015731d27f
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-02 15:51:43 -0600

    add all GPUs

--- src/config.py
+++ src/config.py
@@ -7,10 +7,12 @@ from tensorflow.keras import backend
 2 = INFO and WARNING messages are not printed
 3 = INFO, WARNING, and ERROR messages are not printed
 '''
-# os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+os.environ['TF_CPP_MIN_LOG_LEVEL']='0'
 
 '''
 # channel first -> B C H W
 # channel last  -> B H W C 
 '''
 backend.set_image_data_format('channels_first')
+
+GPUs=["GPU:0", "GPU:1"]
