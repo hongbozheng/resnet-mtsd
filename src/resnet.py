6c4eff9dcb867c4af186b654b95231b4e2f3989e
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-03 15:05:57 -0600

    resnet architecture :white_check_mark:

--- src/resnet.py
+++ src/resnet.py
@@ -14,6 +14,47 @@ INPUT_HEIGHT=224
 INPUT_WIDTH=224
 
 class ResNet():
+    """Instantiates the ResNet, ResNetV2, and ResNeXt architecture.
+    Args:
+      num_res_blocks: List[int], # of residual blocks
+        in each of the 4 layers in ResNet architecture
+      model_name: string, model name.
+      include_top: whether to include the fully-connected
+        layer at the top of the network.
+      weights: one of `None` (random initialization),
+        'imagenet' (pre-training on ImageNet),
+        or the path to the weights file to be loaded.
+      input_tensor: optional Keras tensor
+        (i.e. output of `layers.Input()`)
+        to use as image input for the model.
+      input_shape: optional shape tuple, only to be specified
+        if `include_top` is False (otherwise the input shape
+        has to be `(224, 224, 3)` (with `channels_last` data format)
+        or `(3, 224, 224)` (with `channels_first` data format).
+        It should have exactly 3 inputs channels.
+      pooling: optional pooling mode for feature extraction
+        when `include_top` is `False`.
+        - `None` means that the output of the model will be
+            the 4D tensor output of the
+            last convolutional layer.
+        - `avg` means that global average pooling
+            will be applied to the output of the
+            last convolutional layer, and thus
+            the output of the model will be a 2D tensor.
+        - `max` means that global max pooling will
+            be applied.
+      classes: optional number of classes to classify images
+        into, only to be specified if `include_top` is True, and
+        if no `weights` argument is specified.
+      classifier_activation: A `str` or callable. The activation function to use
+        on the "top" layer. Ignored unless `include_top=True`. Set
+        `classifier_activation=None` to return the logits of the "top" layer.
+        When loading pretrained weights, `classifier_activation` can only
+        be `None` or `"softmax"`.
+      **kwargs: For backwards compatibility only.
+    Returns:
+      A `keras.Model` instance.
+    """
     def __init__(self,
                  num_res_blocks: List[int],
                  model_name: str,
@@ -24,47 +65,6 @@ class ResNet():
                  pooling=None,
                  classes: int=1000,
                  classifier_activation: str="softmax"):
-        """Instantiates the ResNet, ResNetV2, and ResNeXt architecture.
-        Args:
-          num_res_blocks: List[int], # of residual blocks
-            in each of the 4 layers in ResNet architecture
-          model_name: string, model name.
-          include_top: whether to include the fully-connected
-            layer at the top of the network.
-          weights: one of `None` (random initialization),
-            'imagenet' (pre-training on ImageNet),
-            or the path to the weights file to be loaded.
-          input_tensor: optional Keras tensor
-            (i.e. output of `layers.Input()`)
-            to use as image input for the model.
-          input_shape: optional shape tuple, only to be specified
-            if `include_top` is False (otherwise the input shape
-            has to be `(224, 224, 3)` (with `channels_last` data format)
-            or `(3, 224, 224)` (with `channels_first` data format).
-            It should have exactly 3 inputs channels.
-          pooling: optional pooling mode for feature extraction
-            when `include_top` is `False`.
-            - `None` means that the output of the model will be
-                the 4D tensor output of the
-                last convolutional layer.
-            - `avg` means that global average pooling
-                will be applied to the output of the
-                last convolutional layer, and thus
-                the output of the model will be a 2D tensor.
-            - `max` means that global max pooling will
-                be applied.
-          classes: optional number of classes to classify images
-            into, only to be specified if `include_top` is True, and
-            if no `weights` argument is specified.
-          classifier_activation: A `str` or callable. The activation function to use
-            on the "top" layer. Ignored unless `include_top=True`. Set
-            `classifier_activation=None` to return the logits of the "top" layer.
-            When loading pretrained weights, `classifier_activation` can only
-            be `None` or `"softmax"`.
-          **kwargs: For backwards compatibility only.
-        Returns:
-          A `keras.Model` instance.
-        """
 
         if not (weights in {"imagenet", None} or tf.io.gfile.exists(path=weights)):
             raise ValueError(
