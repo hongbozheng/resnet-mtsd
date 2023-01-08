7286589fb2639b265ce828abf90c63e4b21703d6
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-08 14:50:14 -0600

    resnet architecture :white_check_mark:

--- src/resnet.py
+++ src/resnet.py
@@ -15,88 +15,33 @@ INPUT_HEIGHT=224
 INPUT_WIDTH=224
 
 class ResNet(Layer):
-    """Instantiates the ResNet_FPN architecture.
-    Args:
-      num_res_blocks: List[int], # of residual blocks
-        in each of the 4 layers in ResNet architecture
-      model_name: string, model name.
-      include_top: whether to include the fully-connected
-        layer at the top of the network.
-      weights: one of `None` (random initialization),
-        'imagenet' (pre-training on ImageNet),
-        or the path to the weights file to be loaded.
-      input_tensor: optional Keras tensor
-        (i.e. output of `layers.Input()`)
-        to use as image input for the model.
-      input_shape: optional shape tuple, only to be specified
-        if `include_top` is False (otherwise the input shape
-        has to be `(224, 224, 3)` (with `channels_last` data format)
-        or `(3, 224, 224)` (with `channels_first` data format).
-        It should have exactly 3 inputs channels.
-      pooling: optional pooling mode for feature extraction
-        when `include_top` is `False`.
-        - `None` means that the output of the model will be
-            the 4D tensor output of the
-            last convolutional layer.
-        - `avg` means that global average pooling
-            will be applied to the output of the
-            last convolutional layer, and thus
-            the output of the model will be a 2D tensor.
-        - `max` means that global max pooling will
-            be applied.
-      classes: optional number of classes to classify images
-        into, only to be specified if `include_top` is True, and
-        if no `weights` argument is specified.
-      classifier_activation: A `str` or callable. The activation function to use
-        on the "top" layer. Ignored unless `include_top=True`. Set
-        `classifier_activation=None` to return the logits of the "top" layer.
-        When loading pretrained weights, `classifier_activation` can only
-        be `None` or `"softmax"`.
-      **kwargs: For backwards compatibility only.
-    Returns:
-      A `keras.Model` instance.
-    """
-    def __init__(self, num_res_blocks: List[int], include_top: bool, pooling: bool=None, num_classes: int=1000) -> None:
+    def __init__(self, num_res_blocks: List[int], include_top: bool, pooling: str=None, num_classes: int=1000) -> None:
+        """
+        Instantiates the ResNet architecture
+
+        :param num_res_blocks: List[int], # of residual blocks in each of the 4 layers in ResNet architecture
+        :param include_top: bool, whether to include the fully-connected layer at the top of the network.
+        :param pooling: str, optional pooling mode for feature extraction
+                        when `include_top` is `False`.
+                        - `None` means that the output of the model will be
+                            the 4D tensor output of the
+                            last convolutional layer.
+                        - `avg` means that global average pooling
+                            will be applied to the output of the
+                            last convolutional layer, and thus
+                            the output of the model will be a 2D tensor.
+                        - `max` means that global max pooling will
+                            be applied.
+        :param num_classes: int, optional number of classes to classify images into,
+                            only to be specified if `include_top` is True, and
+                            if no `weights` argument is specified.
+        :param **kwargs: For backwards compatibility only.
+        """
         super(ResNet, self).__init__()
         self.num_res_blocks = num_res_blocks
         self.include_top = include_top
         self.pooling = pooling
         self.num_classes = num_classes
-
-        # input_shape = imagenet_utils.obtain_input_shape(input_shape=input_shape, default_size=224, min_size=32,
-        #                                                 data_format=backend.image_data_format(),
-        #                                                 require_flatten=include_top, weights=None)
-        # if input_tensor is None:
-        #     img_input = layers.Input(shape=input_shape)
-        # else:
-        #     if not backend.is_keras_tensor(x=input_tensor):
-        #         img_input = layers.Input(tensor=input_tensor, shape=input_shape)
-        #     else:
-        #         img_input = input_tensor
-
-        # x = layers.ZeroPadding2D(padding=((3,3),(3,3)), name="conv1_pad")(img_input)
-
-        # x = layers.Conv2D(filters=64, kernel_size=(7,7), strides=(2,2), padding="VALID", name="conv1_conv")(x)
-        # x = layers.BatchNormalization(axis=BN_AXIS, epsilon=1.001e-5, name="conv1_bn")(x)
-        # x = layers.Activation("relu", name="conv1_relu")(x)
-
-        # x = layers.ZeroPadding2D(padding=((1,1),(1,1)), name="pool1_pad")(x)
-        # x = layers.MaxPooling2D(pool_size=(3,3), strides=(2,2), name="pool1_pool")(x)
-
-        # x = self._res_blk_stack(x=x, blocks=num_res_blocks[0], filters=64, strides=(1,1), use_bias=True, name="conv2")
-        # x = self._res_blk_stack(x=x, blocks=num_res_blocks[1], filters=128, strides=(2,2), use_bias=True, name="conv3")
-        # x = self._res_blk_stack(x=x, blocks=num_res_blocks[2], filters=256, strides=(2,2), use_bias=True, name="conv4")
-        # x = self._res_blk_stack(x=x, blocks=num_res_blocks[3], filters=512, strides=(2,2), use_bias=True, name="conv5")
-
-        # if include_top:
-        #     x = layers.GlobalAveragePooling2D(data_format=backend.image_data_format(), name="avg_pool")(x)
-        #     imagenet_utils.validate_activation(classifier_activation=classifier_activation, weights=weights)
-        #     x = layers.Dense(units=classes, activation="softmax", name="predictions")(x)
-        # else:
-        #     if pooling == "avg":
-        #         x = layers.GlobalAveragePooling2D(data_format=backend.image_data_format(), name="avg_pool")(x)
-        #     elif pooling == "max":
-        #         x = layers.GlobalMaxPooling2D(data_format=backend.image_data_format(), name="max_pool")(x)
 
         self.padding_3 = layers.ZeroPadding2D(padding=((3,3),(3,3)), name="conv1_pad")
         self.conv7x7 = layers.Conv2D(filters=64, kernel_size=(7,7), strides=(2,2), padding="VALID", name="conv1_conv")
@@ -111,14 +56,6 @@ class ResNet(Layer):
 
         self.dense = layers.Dense(units=self.num_classes, activation="softmax", name="predictions")
 
-        # self.model = Model(inputs=img_input, outputs=x, name=model_name)
-
-        # if weights == "imagenet":
-        #     self.model.load_weights(filepath="../weights/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5",
-        #                             by_name=False, skip_mismatch=False, options=None)
-        # elif weights is not None:
-        #     self.model.load_weights(filepath=weights, by_name=False, skip_mismatch=False, options=None)
-
     def _res_blk_stack(self,
                        x: tf.float32,
                        blocks: int,
@@ -127,16 +64,16 @@ class ResNet(Layer):
                        use_bias: bool=False,
                        name: str=None
                        ) -> tf.float32:
-        """A set of stacked residual blocks.
-        Args:
-          x: input tensor.
-          filters: integer, filters of the bottleneck layer in a block.
-          blocks: integer, blocks in the stacked blocks.
-          stride: stride of the first layer in the first block.
-          use_bias: Boolean, whether the layer uses a bias vector.
-          name: string, stack label.
-        Returns:
-          Output tensor for the stacked blocks.
+        """
+        create a residual block stack
+
+        :param x: tf.float32, input tensor
+        :param blocks: integer, blocks in the stacked residual blocks
+        :param filters: integer, filters of the bottleneck layer in a block
+        :param strides: integer or integer tuple, stride of the layer in the residual block
+        :param use_bias: Boolean, whether the layer uses a bias vector
+        :param name: string, stack label
+        :return: tf.float32, output tensor for the stacked blocks
         """
         x = self._res_blk(x=x, filters=filters, strides=strides, use_bias=use_bias, conv_shortcut=True, name=name + "_block1")
         for i in range(2, blocks+1):
@@ -151,17 +88,17 @@ class ResNet(Layer):
                  conv_shortcut: bool=True,
                  name: str=None
                  ) -> tf.float32:
-        """A residual block.
-        Args:
-          x: input tensor.
-          filters: integer, filters of the bottleneck layer.
-          stride: default 1, stride of the first layer.
-          use_bias: Boolean, whether the layer uses a bias vector.
-          conv_shortcut: default True, use convolution shortcut if True,
-            otherwise identity shortcut.
-          name: string, block label.
-        Returns:
-          Output tensor for the residual block.
+        """
+        create a residual block
+
+        :param x: tf.float32, input tensor
+        :param filters: integer, filters of the bottleneck layer
+        :param strides: integer or integer tuple, stride of the layer
+        :param use_bias: Boolean, whether the layer uses a bias vector
+        :param conv_shortcut: Boolean, use convolution shortcut if True
+                                otherwise identity shortcut
+        :param name: string, block label
+        :return: tf.float32, output tensor for the residual block
         """
         if conv_shortcut:
             shortcut = layers.Conv2D(filters=4*filters, kernel_size=(1,1), strides=strides, padding="VALID", use_bias=use_bias, name=name + "_0_conv")(x)
@@ -185,6 +122,14 @@ class ResNet(Layer):
         return x
 
     def call(self, inputs: tf.float32, training: bool=False):
+        """
+        layer's logic
+
+        :param inputs: Input tensor, or dict/list/tuple of input tensors
+        :param training: Boolean scalar tensor of Python boolean indicating
+                            whether the `call` is meant for training or inference.
+        :return: A tensor or list/tuple of tensors
+        """
         x = self.padding_3(inputs=inputs, training=training)
         x = self.conv7x7(inputs=x, training=training)
         x = self.bn(inputs=x, training=training)
@@ -212,6 +157,19 @@ class ResNet(Layer):
               input_tensor: tf.float32=None,
               input_shape: tf.float32=None
               ) -> Model:
+        """
+        creates a `keras.Model` for ResNet architecture
+
+        :param name: string, model name.
+        :param weights: Boolean, whether to include the fully-connected layer at the top of the network.
+        :param input_tensor:  optional Keras tensor (i.e. output of `layers.Input()`)
+                                to use as image input for the model.
+        :param input_shape: optional shape tuple, only to be specified if `include_top` is False
+                            (otherwise the input shape has to be `(224, 224, 3)` (with `channels_last` data format)
+                            or `(3, 224, 224)` (with `channels_first` data format).
+                            It should have exactly 3 inputs channels.
+        :return: A `keras.Model` instance.
+        """
         if not (weights in {"imagenet", None} or tf.io.gfile.exists(path=weights)):
             raise ValueError(
                 "The `weights` argument should be either "
@@ -250,12 +208,12 @@ def main():
     input_shape = (BATCH_SIZE, INPUT_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH)
 
     '''ResNet-50'''
-    resnet50 = ResNet(num_res_blocks=[3,4,6,3], include_top=False, pooling=None, num_classes=1000)
+    resnet50 = ResNet(num_res_blocks=[3,4,6,3], include_top=False, pooling="avg", num_classes=1000)
     resnet50_backbone = resnet50.model(name="ResNet-50", weights="imagenet", input_tensor=None, input_shape=input_shape[1:])
 
     '''tensorflow.keras.applications.resnet.ResNet50'''
     resnet50_backbone_orig = ResNet50(include_top=False, weights="imagenet", input_tensor=None, input_shape=input_shape[1:],
-                                      pooling=None, classes=1000)
+                                      pooling="avg", classes=1000)
 
     print(resnet50_backbone.summary())
     print("[INFO]: Total # of layers in ResNet-50 (no top) %d" % len(resnet50_backbone.layers))
