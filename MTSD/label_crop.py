0d10a3041590a01fc45b78341e82cd348d74d788
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-10 17:22:34 -0600

    rename script

--- MTSD/label_crop.py
+++ MTSD/label_crop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import List, Dict
+from typing import Dict
 from src.utils import load_label
 from tqdm import tqdm
 import json
@@ -20,13 +20,15 @@ MTSD_FULLY_ANNOTATED_IMAGES_TEST_LABEL="splits/test.txt"
 MTSD_FULLY_ANNOTATED_IMAGES_DIR="mtsd_fully_annotated_images/"
 ANNOTATIONS_FOLDER="annotations/"
 
+# MTSD_FULLY_ANNOTATED_IMAGES_TRAIN_LABELDIR
+
 MTSD_FULLY_ANNOTATED_IMAGES_TRAIN_DIR="mtsd_fully_annotated_images_train/"
 MTSD_FULLY_ANNOTATED_IMAGES_VAL_DIR="mtsd_fully_annotated_images_val/"
 MTSD_FULLY_ANNOTATED_IMAGES_TEST_DIR="mtsd_fully_annotated_images_test/"
 
-MTSD_FULLY_ANNOTATED_IMAGES_TRAIN_CROPPED_DIR="mtsd_fully_annotated_images_train_cropped/"
-MTSD_FULLY_ANNOTATED_IMAGES_VAL_CROPPED_DIR="mtsd_fully_annotated_images_val_cropped/"
-MTSD_FULLY_ANNOTATED_IMAGES_TEST_CROPPED_DIR="mtsd_fully_annotated_images_test_cropped/"
+MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_TRAIN_DIR="mtsd_fully_annotated_cropped_images_train/"
+MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_VAL_DIR="mtsd_fully_annotated_cropped_images_val/"
+MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_TEST_DIR="mtsd_fully_annotated_cropped_images_test/"
 
 VALID_MTSD_FULLY_ANNOTATED_SIGNS_TRAIN_NUM=0
 VALID_MTSD_FULLY_ANNOTATED_SIGNS_VAL_NUM=0
@@ -73,11 +75,11 @@ def update_num_signs(valid: bool, type: str) -> None:
 
 def save_cropped_image(image_cropped: Image, image_name: str, type: str) -> None:
     if type == "train":
-        image_cropped.save(fp=MTSD_FULLY_ANNOTATED_IMAGES_TRAIN_CROPPED_DIR + image_name, format="jpeg")
+        image_cropped.save(fp=MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_TRAIN_DIR + image_name, format="jpeg")
     elif type == "val":
-        image_cropped.save(fp=MTSD_FULLY_ANNOTATED_IMAGES_VAL_CROPPED_DIR + image_name, format="jpeg")
+        image_cropped.save(fp=MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_VAL_DIR + image_name, format="jpeg")
     elif type == "test":
-        image_cropped.save(fp=MTSD_FULLY_ANNOTATED_IMAGES_TEST_CROPPED_DIR + image_name, format="jpeg")
+        image_cropped.save(fp=MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_TEST_DIR + image_name, format="jpeg")
     return
 
 def filter_crop_save(image_label: str, annotation: str, type: str) -> None:
@@ -113,7 +115,7 @@ def main():
     for label in tqdm(train_label):
         annotation_file = ANNOTATIONS_FOLDER + label + ".json"
         filter_crop_save(image_label=label, annotation=annotation_file, type="train")
-    print("[INFO]: Cropped images saved to \"%s\"" % MTSD_FULLY_ANNOTATED_IMAGES_TRAIN_CROPPED_DIR)
+    print("[INFO]: Cropped images saved to \"%s\"" % MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_TRAIN_DIR)
 
     # crop val dataset
     print("[INFO]: Start cropping validation dataset...")
@@ -121,7 +123,7 @@ def main():
     for label in tqdm(val_label):
         annotation_file = ANNOTATIONS_FOLDER + label + ".json"
         filter_crop_save(image_label=label, annotation=annotation_file, type="val")
-    print("[INFO]: Cropped images saved to \"%s\"" % MTSD_FULLY_ANNOTATED_IMAGES_VAL_CROPPED_DIR)
+    print("[INFO]: Cropped images saved to \"%s\"" % MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_VAL_DIR)
 
     # crop test dataset
     print("[INFO]: Start cropping testing dataset...")
@@ -129,7 +131,7 @@ def main():
     for label in tqdm(test_label):
         annotation_file = ANNOTATIONS_FOLDER + label + ".json"
         filter_crop_save(image_label=label, annotation=annotation_file, type="train")
-    print("[INFO]: Cropped images saved to \"%s\"" % MTSD_FULLY_ANNOTATED_IMAGES_TEST_CROPPED_DIR)
+    print("[INFO]: Cropped images saved to \"%s\"" % MTSD_FULLY_ANNOTATED_CROPPED_IMAGES_TEST_DIR)
 
     print("[INFO]: Training Dataset   # %d" % VALID_MTSD_FULLY_ANNOTATED_SIGNS_TRAIN_NUM)
     print("[INFO]: Validation Dataset # %d" % VALID_MTSD_FULLY_ANNOTATED_SIGNS_VAL_NUM)
