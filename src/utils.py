1818821f6085485111022b90445207c7d3c87b80
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-10 15:50:54 -0600

    fix fn syntax

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import config
 
-def load_label(data_label_file):
-    fp = open(data_label_file,'r')
+def load_label(data_label_file: str) -> List:
+    fp = open(data_label_file, 'r')
     label = fp.read().split('\n')[:-1]
     fp.close()
     return label
