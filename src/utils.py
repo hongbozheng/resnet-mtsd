e7adfc229cb3993809bab826a090222cf6c6b421
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-10 18:31:41 -0600

    fix utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,12 @@ from typing import List
 
 def load_label(data_label_file: str) -> List:
     fp = open(data_label_file, 'r')
+    label = fp.read().split('\n')
+    fp.close()
+    return label
+
+def load_label_nl(data_label_file: str) -> List:
+    fp = open(data_label_file, 'r')
     label = fp.read().split('\n')[:-1]
     fp.close()
     return label
