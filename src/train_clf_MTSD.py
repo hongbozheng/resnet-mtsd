dd3e1e34cb0be69a547300cdf7820d48314e51c8
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-10 18:31:58 -0600

    sync

--- src/train_clf_MTSD.py
+++ src/train_clf_MTSD.py
@@ -2,6 +2,7 @@ import config
 from resnet import ResNet
 from resnet_clf import Classifier
 import tensorflow as tf
+from tensorflow.keras.models import Model
 
 MTSD_CLASSES=401
 BATCH_SIZE=1
@@ -11,18 +12,19 @@ INPUT_WIDTH=224
 INPUT_SHAPE = (BATCH_SIZE, INPUT_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH)
 
 def main():
-    input_shape = (BATCH_SIZE, INPUT_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH)
-
-    '''ResNet-50 Backbone'''
-    resnet50 = ResNet(num_res_blocks=[3,4,6,3], include_top=False, pooling="avg", num_classes=1000)
-    resnet50_backbone = resnet50.model(input_shape=input_shape[1:], input_tensor=None, name="ResNet-50-Backbone",
+    # creates ResNet-50 Backbone
+    resnet50 = ResNet(num_res_blocks=[3, 4, 6, 3], include_top=False, pooling="avg", num_classes=1000)
+    resnet50_backbone = resnet50.model(input_shape=INPUT_SHAPE[1:], input_tensor=None, name="ResNet-50-Backbone",
                                        weights="imagenet")
-    '''ResNet-50 Backbone + Classifier'''
+    # creates ResNet-50 + Classifier Model
     classifier = Classifier(resnet_backbone=resnet50_backbone, num_classes=MTSD_CLASSES).model(
-        input_shape=input_shape[1:], input_tensor=None, name="ResNet-50 Classifier")
+        input_shape=INPUT_SHAPE[1:], input_tensor=None, name="ResNet-50 Classifier")
 
     print(classifier.summary())
     print("[INFO]: Total # of layers in ResNet-50 Classifier %d" % len(classifier.layers))
+
+
+
     img_input = tf.random.normal(shape=input_shape, dtype=tf.dtypes.float32)
     print(img_input)
     pred = classifier.call(inputs=img_input)
