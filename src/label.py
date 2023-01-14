d0e6d20f06fa08c3d233dbf7e38ff28be0b26c3c
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-13 19:31:54 -0600

    text working

--- src/label.py
+++ src/label.py
@@ -10,7 +10,7 @@ pygame.init()
 
 # Window Configurations
 FPS=144
-WINDOW_WIDTH=800
+WINDOW_WIDTH=1000
 WINDOW_HEIGHT=800
 """
 pygame.FULLSCREEN    create a fullscreen display
@@ -26,7 +26,7 @@ pygame.HIDDEN        window is opened in hidden mode
 FLAGS=RESIZABLE
 BACKGROUND_COLOR=(192,192,192)
 
-# Button Position
+# Button (Next & Prev) Position
 BTN_NEXT_X=700
 BTN_NEXT_Y=700
 BTN_NEXT_WIDTH=50
@@ -35,35 +35,62 @@ BTN_PREV_X=50
 BTN_PREV_Y=700
 BTN_PREV_WIDTH=50
 BTN_PREV_HEIGHT=30
-
-# Button Font
+# Button (Next & Prev) Font
 BTN_TXT_COLOR=(20,20,20)
 BTN_FONT="Consolas"
 BTN_FONT_SIZE=15
 BTN_FONT_BOLD=False
 BTN_FONT_ITALIC=False
-
-# Button Color
+# Button (Next & Prev) Color
 BUTTON_COLOR=(255,165,0)
 BUTTON_COLOR_HOVER=(245, 203, 167)
 BUTTON_COLOR_PRESSED=(211, 84, 0)
 
-# Class Text Position
-CLASS_TXT_POS_X=WINDOW_WIDTH//2
-CLASS_TXT_POS_Y=35
+# Text Class Index Position
+TXT_CLASS_INDEX_POS_X=35
+TXT_CLASS_INDEX_POS_Y=35
+# Text Class Index Color & Font
+TXT_CLASS_INDEX_COLOR=(0,0,0)
+TXT_CLASS_INDEX_FONT="Consolas"
+TXT_CLASS_INDEX_FONT_SIZE=20
+TXT_CLASS_INDEX_FONT_BOLD=True
+TXT_CLASS_INDEX_FONT_ITALIC=False
 
-# Class Text Color & Font
-CLASS_TXT_COLOR=(255,192,203)
-CLASS_TXT_FONT="Consolas"
-CLASS_TXT_FONT_SIZE=20
-CLASS_TXT_FONT_BOLD=False
-CLASS_TXT_FONT_ITALIC=False
+# Text Cls Idx Position
+TXT_CLS_IDX_POS_X=200
+TXT_CLS_IDX_POS_Y=35
+# Text Cls Idx Color & Font
+TXT_CLS_IDX_COLOR=(255,192,203)
+TXT_CLS_IDX_FONT="Consolas"
+TXT_CLS_IDX_FONT_SIZE=20
+TXT_CLS_IDX_FONT_BOLD=True
+TXT_CLS_IDX_FONT_ITALIC=False
+
+# Text Class Position
+TXT_CLASS_POS_X=35
+TXT_CLASS_POS_Y=75
+# Text Class Color & Font
+TXT_CLASS_COLOR=(0,0,0)
+TXT_CLASS_FONT="Consolas"
+TXT_CLASS_FONT_SIZE=20
+TXT_CLASS_FONT_BOLD=True
+TXT_CLASS_FONT_ITALIC=False
+
+# Text Class Name Position
+TXT_CLASS_NAME_POS_X=200
+TXT_CLASS_NAME_POS_Y=75
+# Text Class Name Color & Font
+TXT_CLASS_NAME_COLOR=(255,192,203)
+TXT_CLASS_NAME_FONT="Consolas"
+TXT_CLASS_NAME_FONT_SIZE=20
+TXT_CLASS_NAME_FONT_BOLD=True
+TXT_CLASS_NAME_FONT_ITALIC=False
 
 # MTSD Dataset
 MTSD_CLASSES=401
 MTSD_FULLY_ANNOTATED_CLASSIFIED_CROPPED_IMAGES_TRAIN_DIR="../MTSD/mtsd_fully_annotated_classified_cropped_images_train/"
 CLASS_DIRS=os.listdir(path=MTSD_FULLY_ANNOTATED_CLASSIFIED_CROPPED_IMAGES_TRAIN_DIR)
-INDEX=0
+CLASS_INDEX=0
 
 # MTSD Dataset Image Position
 IMAGE_WIDTH=100
@@ -82,12 +109,12 @@ class Text:
                  txt_pos: Tuple[int, int],
                  ) -> None:
         self.font = pygame.font.SysFont(name=font, size=font_size, bold=bold, italic=italic)
-        self.txt_surf = self.font.render(txt, True, txt_color)
+        self.txt_surf = self.font.render(txt, False, txt_color)
+        self.txt_pos = txt_pos
         self.txt_rect = self.txt_surf.get_rect(center=txt_pos)
 
     def txtshow(self, window):
-        window.fill(BACKGROUND_COLOR, (self.txt_rect.topleft, self.txt_rect.bottomright))
-        window.blit(self.txt_surf, self.txt_rect)
+        window.blit(self.txt_surf, self.txt_pos)
 
 class Button:
     def __init__(self,
@@ -151,24 +178,42 @@ class LabelTool:
                              btn_txt="Prev", btn_txt_color=BTN_TXT_COLOR,
                              onclick_fn=self.imshow_prev, one_press=False)
         self.buttons = [button_next, button_prev]
-        self.class_txt = Text(font=CLASS_TXT_FONT, font_size=CLASS_TXT_FONT_SIZE,
-                              bold=CLASS_TXT_FONT_BOLD, italic=CLASS_TXT_FONT_ITALIC,
-                              txt=CLASS_DIRS[INDEX], txt_color=CLASS_TXT_COLOR,
-                              txt_pos=(CLASS_TXT_POS_X, CLASS_TXT_POS_Y),)
+        self.txt_class_index = Text(font=TXT_CLASS_INDEX_FONT, font_size=TXT_CLASS_INDEX_FONT_SIZE,
+                                    bold=TXT_CLASS_INDEX_FONT_BOLD, italic=TXT_CLASS_INDEX_FONT_ITALIC,
+                                    txt="Class Index: ", txt_color=TXT_CLASS_INDEX_COLOR,
+                                    txt_pos=(TXT_CLASS_INDEX_POS_X, TXT_CLASS_INDEX_POS_Y))
+        self.txt_cls_idx = Text(font=TXT_CLS_IDX_FONT, font_size=TXT_CLS_IDX_FONT_SIZE,
+                                bold=TXT_CLS_IDX_FONT_BOLD, italic=TXT_CLS_IDX_FONT_ITALIC,
+                                txt=str(CLASS_INDEX), txt_color=TXT_CLS_IDX_COLOR,
+                                txt_pos=(TXT_CLS_IDX_POS_X, TXT_CLS_IDX_POS_Y))
+        self.txt_class = Text(font=TXT_CLASS_FONT, font_size=TXT_CLASS_FONT_SIZE,
+                              bold=TXT_CLASS_FONT_BOLD, italic=TXT_CLASS_FONT_ITALIC,
+                              txt="Class: ", txt_color=TXT_CLASS_COLOR,
+                              txt_pos=(TXT_CLASS_POS_X, TXT_CLASS_POS_Y))
+        self.txt_class_name = Text(font=TXT_CLASS_NAME_FONT, font_size=TXT_CLASS_NAME_FONT_SIZE,
+                                   bold=TXT_CLASS_NAME_FONT_BOLD, italic=TXT_CLASS_NAME_FONT_ITALIC,
+                                   txt=CLASS_DIRS[CLASS_INDEX], txt_color=TXT_CLASS_NAME_COLOR,
+                                   txt_pos=(TXT_CLASS_NAME_POS_X, TXT_CLASS_NAME_POS_Y))
         self.window.fill(color=BACKGROUND_COLOR)
         self.fps = fps
         self.fps_clk = pygame.time.Clock()
         pygame.display.flip()
 
-    def update_cls_txt(self):
-        self.class_txt = Text(font=CLASS_TXT_FONT, font_size=CLASS_TXT_FONT_SIZE,
-                              bold=CLASS_TXT_FONT_BOLD, italic=CLASS_TXT_FONT_ITALIC,
-                              txt=CLASS_DIRS[INDEX], txt_color=CLASS_TXT_COLOR,
-                              txt_pos=(CLASS_TXT_POS_X, CLASS_TXT_POS_Y),)
-        self.class_txt.txtshow(window=self.window)
+    def update_txt(self):
+        self.window.fill(color=BACKGROUND_COLOR)
+        self.txt_class_name = Text(font=TXT_CLASS_NAME_FONT, font_size=TXT_CLASS_NAME_FONT_SIZE,
+                                   bold=TXT_CLASS_NAME_FONT_BOLD, italic=TXT_CLASS_NAME_FONT_ITALIC,
+                                   txt=CLASS_DIRS[CLASS_INDEX], txt_color=TXT_CLASS_NAME_COLOR,
+                                   txt_pos=(TXT_CLASS_NAME_POS_X, TXT_CLASS_NAME_POS_Y))
+        self.txt_cls_idx = Text(font=TXT_CLS_IDX_FONT, font_size=TXT_CLS_IDX_FONT_SIZE,
+                                bold=TXT_CLS_IDX_FONT_BOLD, italic=TXT_CLS_IDX_FONT_ITALIC,
+                                txt=str(CLASS_INDEX), txt_color=TXT_CLS_IDX_COLOR,
+                                txt_pos=(TXT_CLS_IDX_POS_X, TXT_CLS_IDX_POS_Y))
+        self.txt_class_name.txtshow(window=self.window)
+        self.txt_cls_idx.txtshow(window=self.window)
 
     def imshow9x9(self) -> None:
-        class_dir = MTSD_FULLY_ANNOTATED_CLASSIFIED_CROPPED_IMAGES_TRAIN_DIR + CLASS_DIRS[INDEX] + '/'
+        class_dir = MTSD_FULLY_ANNOTATED_CLASSIFIED_CROPPED_IMAGES_TRAIN_DIR + CLASS_DIRS[CLASS_INDEX] + '/'
         if len(class_dir) >= 9:
             pass
         image = pygame.image.load(class_dir + os.listdir(path=class_dir)[0])
@@ -176,18 +221,18 @@ class LabelTool:
         self.window.blit(image, (IMAGE_POS_X, IMAGE_POS_Y))
 
     def imshow_prev(self) -> None:
-        global INDEX
-        if INDEX > 0:
-            INDEX -= 1
+        global CLASS_INDEX
+        if CLASS_INDEX > 0:
+            CLASS_INDEX -= 1
+        self.update_txt()
         self.imshow9x9()
-        self.update_cls_txt()
 
     def imshow_next(self) -> None:
-        global INDEX
-        if INDEX < MTSD_CLASSES-1:
-            INDEX += 1
+        global CLASS_INDEX
+        if CLASS_INDEX < MTSD_CLASSES-1:
+            CLASS_INDEX += 1
+        self.update_txt()
         self.imshow9x9()
-        self.update_cls_txt()
 
     def run(self) -> None:
         while True:
@@ -198,7 +243,8 @@ class LabelTool:
                 elif event.type == VIDEORESIZE:
                     self.window.fill(color=self.background_color)
                     pygame.display.flip()
-            # self.class_txt.txtshow(window=self.window)
+            self.txt_class_index.txtshow(window=self.window)
+            self.txt_class.txtshow(window=self.window)
             for button in self.buttons:
                 button.process(window=self.window)
                 pygame.display.flip()
